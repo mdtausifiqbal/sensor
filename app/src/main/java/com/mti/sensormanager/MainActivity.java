f0c4e1e0398c2f69784d858317c77a697e2e0e56
Md Tausif Iqbal <mdtausifiqbalnwd@gmail.com>
2018-11-12 22:18:42 +0530

    added proximity sensor

--- app/src/main/java/com/mti/sensormanager/MainActivity.java
+++ app/src/main/java/com/mti/sensormanager/MainActivity.java
@@ -1,14 +1,65 @@
 package com.mti.sensormanager;
 
 import android.app.*;
+import android.content.*;
+import android.hardware.*;
 import android.os.*;
+import android.widget.*;
+import java.util.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity implements SensorEventListener
 {
+
+	@Override
+	public void onSensorChanged(SensorEvent p1)
+	{
+		switch(p1.sensor.getType()){
+			case Sensor.TYPE_PROXIMITY:
+				tv.setText(String.format("Proximity sensor %1$.2f",p1.values[0]));
+				break;
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor p1, int p2)
+	{
+		// TODO: Implement this method
+	}
+	
+	private TextView tv;
+	private SensorManager manager;
+	private Sensor proximity;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		tv = (TextView) findViewById(R.id.list);
+		manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		proximity = manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
+
+	@Override
+	protected void onStart()
+	{
+		// TODO: Implement this method
+		super.onStart();
+		if(proximity == null){
+			tv.setText("Not found Proximity sensor");
+		}else{
+			manager.registerListener(this,proximity,SensorManager.SENSOR_DELAY_NORMAL);
+		}
+	}
+
+	@Override
+	protected void onPause()
+	{
+		// TODO: Implement this method
+		super.onPause();
+		if(proximity != null){
+			manager.unregisterListener(this);
+		}
+	}
+	
+	
 }
